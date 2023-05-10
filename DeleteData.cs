ac17967dc73371395d5fbf42851c4be95e1a102f
MayoLayo <l.t05@outlook.com>
2023-05-10 10:57:13 +0200

    Add project files.

---
+++ DeleteData.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data.OleDb;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DataBaseProject
+{
+    public partial class DeleteData : Form
+    {
+        public DeleteData()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            OleDbConnection conn = new OleDbConnection();
+            conn.ConnectionString = "Provider = Microsoft.ace.oledb.12.0; Data Source = Database1.accdb";
+            conn.Open();
+
+            OleDbCommand comm = new OleDbCommand();
+            comm.CommandText = "delete from NameList where Fname = '"+textBox1.Text+"'";
+            comm.Connection = conn;
+            comm.ExecuteNonQuery();
+            conn.Close();
+            MessageBox.Show("Data deleted successfully!");
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+        }
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data.OleDb;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DataBaseProject
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            AddData f = new AddData();
+            f.ShowDialog();
+            
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            ShowData f = new ShowData();
+            f.ShowDialog();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            DeleteData f = new DeleteData();
+            f.ShowDialog();
+        }
+    }
+}
